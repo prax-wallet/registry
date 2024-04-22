3b8395318d40db7ed9794b2f03705f60962580bf
Valentine <walik19988@gmail.com>
2024-04-22 19:07:52 +0000

    testnet-preview helper (#19)
    * testnet-preview helper
    
    * added tests

--- npm/src/client.ts
+++ npm/src/client.ts
@@ -3,6 +3,7 @@ import {
   AssetId,
   Metadata,
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
+import { deriveTestnetChainIdFromPreview, isTestnetPreviewChainId } from './utils';
 
 // @ts-expect-error alias for dev only
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -44,6 +45,12 @@ export class ChainRegistryClient {
   }
 
   async get(chainId: string): Promise<Registry> {
+    if (isTestnetPreviewChainId(chainId)) {
+      const derivedChainId = deriveTestnetChainIdFromPreview(chainId);
+      if (derivedChainId) {
+        return this.github.fetchRegistryData(derivedChainId);
+      }
+    }
     return this.github.fetchRegistryData(chainId);
   }
 
