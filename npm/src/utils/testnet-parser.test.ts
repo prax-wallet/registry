46a22eeea3e61efebc50e04674a689f01941f803
Max Korsunov <maxkors2014@gmail.com>
2024-07-22 16:59:47 +0200

    feat: #62: add support for local devnets

--- npm/src/utils/testnet-parser.test.ts
+++ npm/src/utils/testnet-parser.test.ts
@@ -2,6 +2,14 @@ import { describe, expect, it } from 'vitest';
 import { deriveTestnetChainIdFromPreview, isTestnetPreviewChainId } from './testnet-parser';
 
 describe('testnet-preview helper', () => {
+  it('should correctly identify local testnet chain-id', () => {
+    expect(isTestnetPreviewChainId('penumbra-testnet-deimos-8-f75c2ba6')).toBeTruthy();
+    expect(isTestnetPreviewChainId('penumbra-testnet-deimos-8-f2dbce94')).toBeTruthy();
+    expect(isTestnetPreviewChainId('penumbra-testnet-deimos-0044bb22')).toBeTruthy();
+    expect(isTestnetPreviewChainId('penumbra-testnet-rhea-ff99ee10')).toBeTruthy();
+    expect(isTestnetPreviewChainId('penumbra-testnet-tethy12-b4d8f9a0')).toBeTruthy();
+  });
+
   it('should correctly identify testnet-preview chain-id', () => {
     expect(isTestnetPreviewChainId('penumbra-testnet-deimos-8-xf2dbce94')).toBeTruthy();
     expect(isTestnetPreviewChainId('penumbra-testnet-deimos-x0044bb22')).toBeTruthy();
@@ -13,6 +21,7 @@ describe('testnet-preview helper', () => {
     expect(isTestnetPreviewChainId('penumbra-mainnet')).toBeFalsy();
     expect(isTestnetPreviewChainId('penumbra-testnet-rhea')).toBeFalsy();
     expect(isTestnetPreviewChainId('penumbra-testnet-deimos-6')).toBeFalsy();
+    expect(isTestnetPreviewChainId('penumbra-testnet-deimos-8-abc')).toBeFalsy();
   });
 
   it('should correctly derive testnet chain-id from testnet-preview chain-id', () => {
@@ -23,6 +32,9 @@ describe('testnet-preview helper', () => {
       'penumbra-testnet-rhea',
     );
     expect(deriveTestnetChainIdFromPreview('penumbra-testnet-tethys12-xb4d8f9a0')).toEqual(
+      'penumbra-testnet-tethys12',
+    );
+    expect(deriveTestnetChainIdFromPreview('penumbra-testnet-tethys12-b4d8f9a0')).toEqual(
       'penumbra-testnet-tethys12',
     );
   });
