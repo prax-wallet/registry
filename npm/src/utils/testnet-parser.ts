b195bd399ff5242f4c9bb42a705beea0719aef29
Max Korsunov <maxkors2014@gmail.com>
2024-07-24 13:05:37 +0200

    feat: #62: add support for local devnets (#63)
    * feat: #62: add support for local devnets
    
    * chore: changesets

--- npm/src/utils/testnet-parser.ts
+++ npm/src/utils/testnet-parser.ts
@@ -1,10 +1,13 @@
-// Test preview chain ids end with -x followed by a hexadecimal string
+const testnetChainIdPattern = /-x?[0-9a-f]{8}$/;
+
+/**
+ * Preview chain ids end with '-x' followed by a hexadecimal string of length 8
+ * Local chain ids end with a '-' followed by a hexadecimal string of length 8
+ */
 export const isTestnetPreviewChainId = (chainId: string): boolean => {
-  const previewRegex = /-x[0-9a-f]+$/i;
-  return previewRegex.test(chainId);
+  return testnetChainIdPattern.test(chainId);
 };
 
 export const deriveTestnetChainIdFromPreview = (previewChainId: string): string => {
-  const index = previewChainId.lastIndexOf('-x');
-  return previewChainId.substring(0, index);
+  return previewChainId.substring(0, previewChainId.search(testnetChainIdPattern));
 };
