bd62f5ca78813b618bfeb7aab916bc47c6f7e63e
Conor Schaefer <conor@penumbralabs.xyz>
2024-09-03 15:26:34 -0700

    feat: support github api token
    If set, the GITHUB_TOKEN env var will be used to provide
    bearer-authentication for the github API. If not set,
    the requests will still be made, but much more likely to fail via HTTP
    403/429, due to ratelimiting on unauthenticated requests.
    See docs at https://docs.github.com/en/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28

--- tools/compiler/src/main.rs
+++ tools/compiler/src/main.rs
@@ -4,5 +4,9 @@ use penumbra_registry::processor::generate_registry;
 #[tokio::main]
 async fn main() -> AppResult<()> {
     tracing_subscriber::fmt::init();
+
+    if std::env::var("GITHUB_TOKEN").is_err() {
+        tracing::warn!("GITHUB_TOKEN env var not set, requests may fail due to API ratelimiting");
+    }
     generate_registry().await
 }
